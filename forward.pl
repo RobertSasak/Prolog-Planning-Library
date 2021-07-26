986a526cb4acb8493faa90f06f637b363fba5df0
Tobias Opsahl <tobiasop@gmail.com>
2021-07-26 17:27:47 +0200

    remove unecessary comments

--- forward.pl
+++ forward.pl
@@ -14,26 +14,13 @@ make_init_state(I):-
 		bb_put(fictiveGoal, G).
 
 
-make_solution(S, S).
-		
-%step(State, ActionDef, NewState):-
-%		get_action(A, ActionDef),
-%		get_precondition(A, P),    mysubset(P, State),	% choose suitable action
-%		get_negativ_effect(A, NE), ord_subtract(State, NE, State2),	
-%		get_positiv_effect(A, PE), ord_union(State2, PE, NewState).
+make_solution(S, S).		
 
 step(State, ActionDef, NewState):-
 		get_action(A, ActionDef),
 		get_precondition(A, P1), sort(P1, P), mysubset(P, State),	% choose suitable action
 		get_negativ_effect(A, NE1), sort(NE1, NE), ord_subtract(State, NE, State2),
 		get_positiv_effect(A, PE1), sort(PE1, PE), ord_union(State2, PE, NewState).
-%		print('State: '), print(State), nl,
-%		print('Precondition: '), print(P), nl,
-%	  	print('Positive effect: '), print(PE), nl,
-%		print('Negative effect: '), print(NE), nl,
-%		print('State2: '), print(State2), nl,
-%		print('NewState: '), print(NewState), nl, nl.
-
 
 is_goal(S):-
 		get_goal(G),
